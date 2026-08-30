3472d89b67a49cee059c2a5971b1fb95c2804b8a
apgsm <a1a21923@outlook.com>
2026-08-30 19:26:26 +0800

    Initial commit

--- Git更新.bat
+++ Git更新.bat
@@ -1,16 +1,11 @@
 @echo off
-chcp 65001 >nul
-:: 直接切换盘符+目录
 cd /d "D:\网站\个人主页"
-
-:: 判断目录是否真实存在
 if not exist . (
-    echo 【错误】目录 D:\网站\个人主页 不存在！检查文件夹路径！
+    echo ERROR: Directory not found!
     pause
     exit /b
 )
-
-echo 当前工作目录：%cd%
+echo Current directory: %cd%
 echo =====================================
 
 git init
@@ -20,6 +15,5 @@ git remote remove origin 2>nul
 git remote add origin git@github.com:apgsm/apgsm.qzz.io.git
 git push -u origin main
 
-echo.
-echo 执行完毕，请查看上面输出有无报错
+echo Operation finished. Check above logs for errors.
 pause
